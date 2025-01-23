98ba64efc5433b6ab6e5202106628671e395ab51
StarAssassin64 <starassassin64@starassassin64.com>
2025-01-22 19:22:34 -0600

    modfied comments

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 struct GamepadInfo {
     gpd_button0 : u8,    // Usage 0x00090001: Button 0 Primarry, Value = 0 to 1, Physical = Value
     gpd_button1 : u8,    // Usage 0x00090002: Button 1 Seconday, Value = 0 to 1, Physical = Value 
-    gpd_button2 : u8,    // Usage 0x00090003: Button 1 Seconday, Value = 0 to 1, Physical = Value
-    gpd_button3 : u8,    // Usage 0x00090004: Button 1 Seconday, Value = 0 to 1, Physical = Value
-    gpd_button4 : u8,    // Usage 0x00090005: Button 1 Seconday, Value = 0 to 1, Physical = Value
-    gpd_button5 : u8,    // Usage 0x00090006: Button 1 Seconday, Value = 0 to 1, Physical = Value
-    gpd_button6 : u8,    // Usage 0x00090007: Button 1 Seconday, Value = 0 to 1, Physical = Value
-    gpd_button7 : u8,    // Usage 0x00090008: Button 1 Seconday, Value = 0 to 1, Physical = Value
-    gpd_button8 : u8,    // Usage 0x00090009: Button 1 Seconday, Value = 0 to 1, Physical = Value
-    gpd_button9 : u8,    // Usage 0x0009000a: Button 1 Seconday, Value = 0 to 1, Physical = Value
-    gpd_button10 : u8,   // Usage 0x0009000b: Button 1 Seconday, Value = 0 to 1, Physical = Value
-    gpd_button11 : u8,   // Usage 0x0009000c: Button 1 Seconday, Value = 0 to 1, Physical = Value
-    gpd_button12 : u8,   // Usage 0x0009000d: Button 1 Seconday, Value = 0 to 1, Physical = Value
-    gpd_button13 : u8,   // Usage 0x0009000e: Button 1 Seconday, Value = 0 to 1, Physical = Value
-    gpd_button14 : u8,   // Usage 0x0009000f: Button 1 Seconday, Value = 0 to 1, Physical = Value
+    gpd_button2 : u8,    // Usage 0x00090003: Button 2 Seconday, Value = 0 to 1, Physical = Value
+    gpd_button3 : u8,    // Usage 0x00090004: Button 3 Seconday, Value = 0 to 1, Physical = Value
+    gpd_button4 : u8,    // Usage 0x00090005: Button 4 Seconday, Value = 0 to 1, Physical = Value
+    gpd_button5 : u8,    // Usage 0x00090006: Button 5 Seconday, Value = 0 to 1, Physical = Value
+    gpd_button6 : u8,    // Usage 0x00090007: Button 6 Seconday, Value = 0 to 1, Physical = Value
+    gpd_button7 : u8,    // Usage 0x00090008: Button 7 Seconday, Value = 0 to 1, Physical = Value
+    gpd_button8 : u8,    // Usage 0x00090009: Button 8 Seconday, Value = 0 to 1, Physical = Value
+    gpd_button9 : u8,    // Usage 0x0009000a: Button 9 Seconday, Value = 0 to 1, Physical = Value
+    gpd_button10 : u8,   // Usage 0x0009000b: Button 10 Seconday, Value = 0 to 1, Physical = Value
+    gpd_button11 : u8,   // Usage 0x0009000c: Button 11 Seconday, Value = 0 to 1, Physical = Value
+    gpd_button12 : u8,   // Usage 0x0009000d: Button 12 Seconday, Value = 0 to 1, Physical = Value
+    gpd_button13 : u8,   // Usage 0x0009000e: Button 13 Seconday, Value = 0 to 1, Physical = Value
+    gpd_button14 : u8,   // Usage 0x0009000f: Button 14 Seconday, Value = 0 to 1, Physical = Value
     gpd_hatswitch : u8,  // Usage 0x00010039: Hat Switch, Value = 0 to 7, Physical = Value x 45
 }
 
